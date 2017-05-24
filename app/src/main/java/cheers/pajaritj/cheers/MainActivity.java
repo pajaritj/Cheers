ca1d84b89ee63a3eaf3e3da0b34b888ab1e19d03
Jessa Pajarito <pajaritj@tcd.ie>
2017-05-24 20:03:56 +0100

    Fixed max_pages error

--- app/src/main/java/cheers/pajaritj/cheers/MainActivity.java
+++ app/src/main/java/cheers/pajaritj/cheers/MainActivity.java
@@ -3,6 +3,7 @@ package cheers.pajaritj.cheers;
 
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -39,6 +40,8 @@ public class MainActivity extends AppCompatActivity {
 
         private final static String API_KEY = "0e94bfe6b6502157183e836c3ddfc61f";
 
+        private final int CHEERS_LIMIT = 15;
+
         private TextView beer_description,beer_name;
 
         private ImageView beer_label;
@@ -51,21 +54,28 @@ public class MainActivity extends AppCompatActivity {
 
         List <BeerResponse.Beer> beers;
 
-        int current_beer=1;
-
+        private int current_beer=1;
+        private int current_page =1;
+        private int max_page=1;
+        private int cheers_counter = 0;
+        private int data_counter =1;
+        private int glass_value=5;
         @Override
         protected void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
             setContentView(R.layout.activity_main);
+            Typeface custom_font = Typeface.createFromAsset(getAssets(),  "fonts/Draft Beer.ttf");
             beer_description = (TextView) findViewById(R.id.Beer_Description);
             beer_name = (TextView) findViewById(R.id.Beer_Name);
+            beer_name.setTypeface(custom_font);
             beer_label = (ImageView) findViewById(R.id.Beer_Label);
             cheers_button= (Button) findViewById(R.id.Cheers_Button);
             rand = new Random();
+            glass_value = rand.nextInt(14)+1;
             Map <String,String> query_data =new HashMap<>();
-            query_data.put("glasswareId",String.valueOf(5));
+            query_data.put("glasswareId",String.valueOf(glass_value));
             query_data.put("key",API_KEY);
-            query_data.put("p",String.valueOf(1));
+            query_data.put("p",String.valueOf(current_page));
 
             if (API_KEY.isEmpty()) {
                 Toast.makeText(getApplicationContext(), "Please obtain your API KEY first from BreweryDB", Toast.LENGTH_LONG).show();
@@ -78,13 +88,17 @@ public class MainActivity extends AppCompatActivity {
                  @Override
                  public void onResponse(Call<BeerResponse> call, Response<BeerResponse> response) {
                      Log.d("TAG",response.code()+"");
-                     String displayResponse = "";
                      BeerResponse resource = response.body();
                      beers=resource.data;
+                     max_page=resource.numberOfPages ==null? current_page : resource.numberOfPages;
                      beer_description.setText(beers.get(current_beer).description);
                      beer_name.setText(beers.get(current_beer).nameDisplay);
-                     Picasso.with(MainActivity.this).load(beers.get(current_beer).labels.large).into(beer_label);
-                     beers.remove(current_beer);
+                     if(beers.get(current_beer).labels==null) {
+                        beer_label.setImageResource(R.mipmap.noimg);
+                     }
+                     else{
+                         Picasso.with(MainActivity.this).load(beers.get(current_beer).labels.large).into(beer_label);
+                     }
                  }
 
                  @Override
@@ -95,11 +109,43 @@ public class MainActivity extends AppCompatActivity {
 
             cheers_button.setOnClickListener(new View.OnClickListener() {
                 public void onClick(View v) {
-                    current_beer= rand.nextInt(beers.size()) + 1;
+                    cheers_counter += 1;
+                    current_beer = rand.nextInt(beers.size());
                     beer_description.setText(beers.get(current_beer).description);
                     beer_name.setText(beers.get(current_beer).nameDisplay);
-                    Picasso.with(MainActivity.this).load(beers.get(current_beer).labels.large).into(beer_label);
-                    beers.remove(current_beer);
+                    if (beers.get(current_beer).labels == null) {
+                        beer_label.setImageResource(R.mipmap.noimg);
+                    } else {
+                        Picasso.with(MainActivity.this).load(beers.get(current_beer).labels.large).into(beer_label);
+                    }
+                    if(cheers_counter==CHEERS_LIMIT*data_counter) {
+                        Map <String,String> query_data =new HashMap<>();
+                        query_data.put("glasswareId",String.valueOf(glass_value));
+                        query_data.put("key",API_KEY);
+                        current_page = current_page<max_page ? (current_page+1) : max_page;
+                        query_data.put("p",String.valueOf(current_page));
+                        apiService.getBeers(query_data).enqueue(new Callback<BeerResponse>() {
+                            @Override
+                            public void onResponse(Call<BeerResponse> call, Response<BeerResponse> response) {
+                                Log.d("TAG",response.code()+"");
+                                BeerResponse resource = response.body();
+                                beers=resource.data;
+                                beer_description.setText(beers.get(current_beer).description);
+                                beer_name.setText(beers.get(current_beer).nameDisplay);
+                                if(beers.get(current_beer).labels==null) {
+                                    beer_label.setImageResource(R.mipmap.noimg);
+                                }
+                                else{
+                                    Picasso.with(MainActivity.this).load(beers.get(current_beer).labels.large).into(beer_label);
+                                }
+                            }
+
+                            @Override
+                            public void onFailure(Call<BeerResponse> call, Throwable t) {
+                                Log.e(TAG, t.toString());
+                            }
+                        });
+                    }
                 }
             });
 
